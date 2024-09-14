623643ebb7c5a64f2d303e6fa189f7987f0b4271
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-09-14 19:44:00 +0200

    section styled

--- src/features/PersonalHomepage/Section/styled.js
+++ src/features/PersonalHomepage/Section/styled.js
@@ -5,17 +5,28 @@ export const Wrapper = styled.div`
   box-shadow: 0px 16px 58px 0px #090A3308;
   margin: auto;
   max-width: 1216px;
+  padding: 32px;
+  margin-bottom: 72px;
 `;
 
 export const Header = styled.h2`
   font-weight: 900;
   font-size: 30px;
   border-bottom: 1px solid #D1D5DA4D;
-  margin: 32px;
+  margin: 0;
   max-width: 1151px;
+  padding-bottom: 13px;
 `;
 
 export const List = styled.ul`
-  margin: 32px;
   max-width: 1151px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25.2px;
+  margin: 0;
+  padding-top: 32px;
 `;
+
+export const ListItem = styled.li``;
