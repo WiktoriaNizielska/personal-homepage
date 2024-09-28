8158ff1c07441757577ff9f120bd5ce0ef3a1714
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-09-28 20:24:00 +0200

    getRepositories function created

---
+++ src/features/PersonalHomepage/Portfolio/Projects/getRepositories.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getRepositories = async () => {
+  try {
+    const response = await axios.get("https://api.github.com/users/WiktoriaNizielska/repos");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
