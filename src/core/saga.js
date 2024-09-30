d484ffbf0d69fe819db5b906c46d14051aa0259a
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-09-30 13:19:21 +0200

    saga created

---
+++ src/core/saga.js
@@ -0,0 +1,6 @@
+import { all } from "redux-saga/effects";
+import { portfolioSaga } from "../features/PersonalHomepage/Portfolio/Projects/portfolioSaga";
+
+export default function* saga() {
+  yield all([portfolioSaga()]);
+}
