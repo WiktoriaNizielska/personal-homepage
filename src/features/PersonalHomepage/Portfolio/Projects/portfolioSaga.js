6bb1a9af40c451fa37f8ffbb3c4e0e0162987450
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-09-30 12:45:03 +0200

    portfolioSaga created

---
+++ src/features/PersonalHomepage/Portfolio/Projects/portfolioSaga.js
@@ -0,0 +1,17 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+import { getRepositories } from "./getRepositories";
+import { fetchProjects, fetchProjectsError, fetchProjectsSuccess } from "./portfolioSlice";
+
+function* fetchProjectsHandler() {
+  try {
+    yield delay(2000);
+    const project = yield call(getRepositories);
+    yield put(fetchProjectsSuccess(project));
+  } catch (error) {
+    yield put(fetchProjectsError(error));
+  }
+}
+
+export function* portfolioSaga() {
+  yield takeEvery(fetchProjects.type, fetchProjectsHandler)
+}
