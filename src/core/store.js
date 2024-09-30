d707f30b964b032b419cf2084765d6d60c49b2c1
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-09-30 13:27:15 +0200

    saga conected to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import portfolioReducer from "../features/PersonalHomepage/Portfolio/Projects/portfolioSlice";
+import createSagaMiddleware from "redux-saga";
+import saga from "./saga";
 
-export default configureStore({
-  reducer:{
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {
     portfolio: portfolioReducer,
-  }
-})
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(saga);
+
+export default store;
