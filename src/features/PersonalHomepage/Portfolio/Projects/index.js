08295713f9ce6b7959b1fbe786dd688041bef221
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-01 11:01:52 +0200

    styled components added to Projects component

--- src/features/PersonalHomepage/Portfolio/Projects/index.js
+++ src/features/PersonalHomepage/Portfolio/Projects/index.js
@@ -1,5 +1,16 @@
-export const Projects = () => (
-  <>
+import { Description, Link, LinkContainer, LinkTitle, ProjectTitle, Tile, Wrapper } from "./styled";
 
-  </>
+export const Projects = ({ title, description, demoLink, codeLink }) => (
+  <Wrapper>
+    <Tile>
+      <ProjectTitle>{title}</ProjectTitle>
+      <Description>{description}</Description>
+      <LinkContainer>
+        <LinkTitle>Demo:</LinkTitle>
+        <Link>{demoLink}</Link>
+        <LinkTitle>Code:</LinkTitle>
+        <Link>{codeLink}</Link>
+      </LinkContainer>
+    </Tile>
+  </Wrapper>
 );
---
+++ src/features/PersonalHomepage/Portfolio/Projects/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+`;
+
+export const Tile = styled.div`
+
+`;
+
+export const ProjectTitle = styled.h3`
+
+`;
+
+export const Description = styled.p`
+
+`;
+
+export const LinkContainer = styled.div`
+
+`;
+
+export const LinkTitle = styled.p`
+
+`;
+
+export const Link = styled.a`
+
+`;
