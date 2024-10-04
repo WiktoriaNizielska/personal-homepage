7eeffff9909de1c95687a299c2e4828abac574f6
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-04 14:17:52 +0200

    button toggles theme

--- src/core/App.js
+++ src/core/App.js
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./theme";
+import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
 import { PersonalHomepage } from "../features/PersonalHomepage";
+import { useSelector } from "react-redux";
+import { selectDarkMode } from "../features/PersonalHomepage/ThemeButton/themeSlice";
 
 function App() {
+  const darkMode = useSelector(selectDarkMode);
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyle />
       <PersonalHomepage />
     </ThemeProvider>
--- src/features/PersonalHomepage/ThemeButton/index.js
+++ src/features/PersonalHomepage/ThemeButton/index.js
@@ -1,10 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Text, Wrapper, Button } from "./styled";
+import { selectDarkMode, toggleTheme } from "./themeSlice";
 
 export const ThemeButton = () => {
+  const dispatch = useDispatch();
+  const { darkMode } = useSelector(selectDarkMode);
+
+
   return (
     <Wrapper>
-      <Text></Text>
-      <Button></Button>
+      <Text>Dark mode {darkMode ? "on" : "off"}</Text>
+      <Button
+        onClick={() => dispatch(toggleTheme())}
+      >
+      </Button>
     </Wrapper>
   );
 };
