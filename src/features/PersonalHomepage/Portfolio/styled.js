ed9f0ac2710819401346b6a928608d79c6f72afa
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-04 11:30:45 +0200

    grid-template-rows deleted

--- src/features/PersonalHomepage/Portfolio/styled.js
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(3, 1fr);
   gap: 32px;
   margin: 24px 0 120px;
 `;
