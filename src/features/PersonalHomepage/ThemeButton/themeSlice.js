3aee3abbf4743854e030104e0363fc525e61c164
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-04 13:36:07 +0200

    themeSlice created

---
+++ src/features/PersonalHomepage/ThemeButton/themeSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initaialState: {
+    darkMode: false,
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      state.darkMode = !state.darkMode;
+    },
+  },
+});
+
+export const { toggleTheme } = themeSlice.actions;
+
+export const selectThemeState = (state) => state.theme;
+
+export const selectDarkMode = (state) => selectThemeState(state).darkMode;
+
+export default themeSlice.reducer;
