314de35738710c5541c61feadec84b54098d2b93
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-16 10:49:33 +0200

    margin changed on smaller screens

--- src/features/PersonalHomepage/Container/styled.js
+++ src/features/PersonalHomepage/Container/styled.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
   max-width: 1216px;
   margin: 119px auto 109px auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tabletMax}) {
+    margin: 34px auto 31px auto;
+  }
 `;
